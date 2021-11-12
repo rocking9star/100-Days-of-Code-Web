3ac93ece4a27e20fc6b24c5be190eb92d2bb2320
NoaKo <noako.js@gmail.com>
2021-11-12 23:48:47 +0200

    playing with js document and query

--- js-exercise-using-document-element/app.js
+++ js-exercise-using-document-element/app.js
@@ -7,5 +7,17 @@ function countChars(event){
     let enteredText=event.target.value;
     let textlength=enteredText.length;
     remainingChars.textContent=maxAllowedChars-textlength;
+
+    if(maxAllowedChars-textlength<=10)
+    {
+        remainingChars.style.color="red";
+        document.querySelector("button").classList="warning";
+    }
+    else
+    {
+        remainingChars.style.color="black";
+        document.querySelector("button").classList.remove("warning");
+    }
+       
 }
 productName.addEventListener('input',countChars);
